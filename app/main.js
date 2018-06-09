5e85c6e0c3fce32660ba4818e256f243c86beee3
Briana Wright <brianametta@gmail.com>
2018-06-09 12:48:57 -0700

    Created separate file for routing, changing css for profile image

---
+++ app/main.js
@@ -0,0 +1,19 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "app/components/about.htm"
+    })
+    .when("/experience", {
+        templateUrl : "app/components/experience.htm"
+    })
+    .when("/education", {
+        templateUrl : "app/components/education.htm"
+    })
+    .when("/skills", {
+        templateUrl : "app/components/skills.htm"
+    })
+    .when("/interests", {
+        templateUrl : "app/components/interests.htm"
+    });
+});
