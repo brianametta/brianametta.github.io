cf3e91f8f82c10b88219266c1139751cf529ee3f
Briana Wright <brianametta@gmail.com>
2018-06-17 09:02:24 -0700

    Adding experience and skills

--- app/portfolioCtrl.js
+++ app/portfolioCtrl.js
@@ -2,6 +2,9 @@ app.controller('portfolioCtrl', [function(){
     var vm = this;
     //I know I should try not to use jQuery, will update it when I have time :)
     $(document).ready(function() {
+        $('.carousel').carousel({
+            interval: 3000
+          })
         if($(window).width() >= 768) {
             $("#wrapper").addClass("toggled");
             $("#topNavigation").hide();
