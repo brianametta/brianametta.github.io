0ccde490e46fd1d7c7a023a28ba3d5f19670b0f8
Briana Wright <brianametta@gmail.com>
2018-06-17 05:57:49 -0700

    Fixing indentation, removing unused code in main controller

--- app/portfolioCtrl.js
+++ app/portfolioCtrl.js
@@ -1,37 +1,25 @@
 app.controller('portfolioCtrl', [function(){
     var vm = this;
-    // Setting the side menu to auto-show, not sure if I want toggling yet
-    // $( document ).ready(function() {
-    //     $("#wrapper").toggleClass("toggled");
-    // });
-    // $("#menu-toggle").click(function(e) {
-    //     e.preventDefault();
-    //     $("#wrapper").toggleClass("toggled");
-    // });
-    function showTopNavigation(state){
-        console.log("called");
-        vm.showTopNav = state;
-    }
-
+    //I know I should try not to use jQuery, will update it when I have time :)
     $(document).ready(function() {
         if($(window).width() >= 768) {
-                $("#wrapper").addClass("toggled");
-                $("#topNavigation").hide();
-            } else if($(window).width() < 768){
-                $("#wrapper").removeClass("toggled");
-                $("#topNavigation").show();
-            }
-        // This will fire when document is ready:
+            $("#wrapper").addClass("toggled");
+            $("#topNavigation").hide();
+        }
+        else if($(window).width() < 768){
+            $("#wrapper").removeClass("toggled");
+            $("#topNavigation").show();
+        }
         $(window).resize(function() {
             // This will fire each time the window is resized:
             if($(window).width() >= 768) {
                 $("#wrapper").addClass("toggled");
                 $("#topNavigation").hide();
-            } else if($(window).width() < 768){
+            }
+            else if($(window).width() < 768){
                 $("#wrapper").removeClass("toggled");
                 $("#topNavigation").show();
             }
-            }); // This will simulate a resize to trigger the initial run.
-        });  
-    }
-]);
+        });
+    });  
+}]);
