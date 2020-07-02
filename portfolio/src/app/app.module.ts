7533b2d8f36ba97ac557d60b0cbfb1554966ba4a
Briana Wright <brianametta@gmail.com>
2020-07-02 15:11:54 -0700

    Adding intro and about section

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, IntroComponent, AboutComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
